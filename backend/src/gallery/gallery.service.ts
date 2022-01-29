a7527a7052406bd57ea2613687688e1cf84eacd4
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-01-29 18:14:54 +0000

    feature: added create item on gallery service

--- backend/src/gallery/gallery.service.ts
+++ backend/src/gallery/gallery.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { DataService } from 'src/core/data/data.service';
+import { CreateGalleryItem } from './dto/create-gallery-item.dto';
 
 @Injectable()
-export class GalleryService {}
+export class GalleryService {
+  constructor(private readonly dataService: DataService) {}
+
+  addItem(data: CreateGalleryItem) {
+    return this.dataService.user.create({ data });
+  }
+}
