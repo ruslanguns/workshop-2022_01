a99f7fa43d3490accbe85fd642db078541bde6c1
Antonio Franco <antoniofranco.dev@gmail.com>
2022-01-29 13:47:17 -0300

    add details component

--- frontend/src/app/details/details.module.ts
+++ frontend/src/app/details/details.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DetailsRoutingModule } from './details-routing.module';
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailsComponent
+  ],
   imports: [
     CommonModule,
     DetailsRoutingModule
