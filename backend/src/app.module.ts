fb692987cd97301e566a82bdefd525e8421c7c30
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-01-29 17:57:19 +0000

    improv: removed unnecessary files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GalleryModule } from './gallery/gallery.module';
 import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [GalleryModule, CoreModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
